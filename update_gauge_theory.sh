601054780191ab9257d45c8806b3c6bcfdd06645
Greg Langmead <glangmead@gmail.com>
2023-09-11 21:47:02 -0400

    rebuilding site Mon Sep 11 21:47:02 EDT 2023

--- update_gauge_theory.sh
+++ update_gauge_theory.sh
@@ -3,7 +3,7 @@ SRCMD=../writing/differential_geometry_in_hott/agda/group_extensions.lagda.md
 TRG=content/writing/towards_gauge_theory_in_hott/index.html
 cp ../writing/differential_geometry_in_hott/agda/html/* content/writing/towards_gauge_theory_in_hott
 echo "---" > $TRG
-echo "title: Towards Gauge Theory in Homotopy Type Theory" >> $TRG
+echo 'title: "$G\\to E\\to B$: Towards Gauge Theory in Homotopy Type Theory"' >> $TRG
 cat $SRCMD | head -n 20 | egrep '^[a-z][a-z]*: ' | egrep -v title >> $TRG
 echo "---" >> $TRG
 cat $SRCHTML | egrep -v "<title>" >> $TRG
