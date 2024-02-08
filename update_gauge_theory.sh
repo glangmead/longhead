62df94c5fa3e6385dc2b7b7a4457ae034438daef
Greg Langmead <glangmead@gmail.com>
2024-02-08 17:28:18 -0500

    rebuilding site Thu Feb  8 17:28:18 EST 2024

--- update_gauge_theory.sh
+++ update_gauge_theory.sh
@@ -1,9 +1,9 @@
-SRCHTML=../writing/differential_geometry_in_hott/agda/html/discrete_gauge_theory.html
-SRCMD=../writing/differential_geometry_in_hott/agda/discrete_gauge_theory.lagda.md
+SRCHTML=../writing/differential_geometry_in_hott/latex/discrete_gauge_theory/html/discrete_gauge_theory.html
+SRCYML=../writing/differential_geometry_in_hott/latex/discrete_gauge_theory/discrete_gauge_theory.yml
 TRG=content/writing/towards_gauge_theory_in_hott/index.html
-cp ../writing/differential_geometry_in_hott/agda/html/* content/writing/towards_gauge_theory_in_hott
+cp ../writing/differential_geometry_in_hott/latex/discrete_gauge_theory/html/* content/writing/towards_gauge_theory_in_hott
 echo "---" > $TRG
 echo 'title: "Discrete Gauge Theory in Homotopy Type Theory"' >> $TRG
-cat $SRCMD | head -n 20 | egrep '^[a-z][a-z]*: ' | egrep -v title >> $TRG
+cat $SRCYML | head -n 20 | egrep '^[a-z][a-z]*: ' | egrep -v title >> $TRG
 echo "---" >> $TRG
 cat $SRCHTML | egrep -v "<title>" >> $TRG
