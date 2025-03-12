0a703a8c057c32ec10091c9037311850b3655c08
Greg Langmead <glangmead@gmail.com>
2025-03-12 10:10:00 -0400

    rebuilding site Wed Mar 12 10:10:00 EDT 2025

--- update_gauge_theory.sh
+++ update_gauge_theory.sh
@@ -5,6 +5,6 @@ cp ../writing/differential_geometry_in_hott/latex/discrete_gauge_theory/html/* c
 cp ../writing/differential_geometry_in_hott/latex/discrete_gauge_theory/discrete_gauge_theory.pdf content/writing/towards_gauge_theory_in_hott/towards_gauge_theory_in_hott.pdf
 echo "---" > $TRG
 echo 'title: "Discrete differential geometry in homotopy type theory"' >> $TRG
-cat $SRCYML | head -n 20 | egrep '^[a-z][a-z]*: ' | egrep -v title >> $TRG
+cat $SRCYML | egrep '^[a-z][a-z]*: ' | egrep -v title >> $TRG
 echo "---" >> $TRG
 cat $SRCHTML | egrep -v "(<title>|<h1 class=.title|<p class=.author|<p class=.date)" >> $TRG
