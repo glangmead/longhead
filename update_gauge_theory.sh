784a7159ae669e40e1f1b2b3f6c6e83f0810ada2
Greg Langmead <glangmead@gmail.com>
2025-04-05 14:53:25 -0400

    rebuilding site Sat Apr  5 14:53:25 EDT 2025

--- update_gauge_theory.sh
+++ update_gauge_theory.sh
@@ -2,9 +2,10 @@ SRCHTML=../writing/differential_geometry_in_hott/latex/discrete_gauge_theory/htm
 SRCYML=../writing/differential_geometry_in_hott/latex/discrete_gauge_theory/discrete_gauge_theory.yml
 TRG=content/writing/towards_gauge_theory_in_hott/index.html
 cp ../writing/differential_geometry_in_hott/latex/discrete_gauge_theory/html/* content/writing/towards_gauge_theory_in_hott
-cp ../writing/differential_geometry_in_hott/latex/discrete_gauge_theory/discrete_gauge_theory.pdf content/writing/towards_gauge_theory_in_hott/towards_gauge_theory_in_hott.pdf
+cp ../writing/differential_geometry_in_hott/latex/discrete_gauge_theory/discrete_gauge_theory.pdf content/writing/towards_gauge_theory_in_hott/
+cp ../writing/differential_geometry_in_hott/latex/discrete_gauge_theory/dgt_talk.pdf content/writing/towards_gauge_theory_in_hott/
 echo "---" > $TRG
 echo 'title: "Discrete differential geometry in homotopy type theory"' >> $TRG
 cat $SRCYML | egrep '^[a-z][a-z]*: ' | egrep -v title >> $TRG
 echo "---" >> $TRG
-cat $SRCHTML | egrep -v "(<title>|<h1 class=.title|<p class=.author|<p class=.date)" >> $TRG
+cat $SRCHTML | sed 's+<body>+<body><p><b>Download <a href="discrete_gauge_theory.pdf">the PDF version</a> or <a href="dgt_talk.pdf">the slides</a></b>.</p>+' | egrep -v "(<title>|<h1 class=.title|<p class=.author|<p class=.date)" >> $TRG
