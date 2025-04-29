12aee45a7441fcd8b09521d968643857c1942973
Greg Langmead <glangmead@gmail.com>
2025-04-28 21:37:47 -0400

    rebuilding site Mon Apr 28 21:37:47 EDT 2025

--- update_gauge_theory.sh
+++ update_gauge_theory.sh
@@ -1,10 +1,10 @@
 SRCHTML=../writing/differential_geometry_in_hott/latex/discrete_gauge_theory/html/discrete_gauge_theory.html
 SRCYML=../writing/differential_geometry_in_hott/latex/discrete_gauge_theory/discrete_gauge_theory.yml
-TRG=content/writing/towards_gauge_theory_in_hott/index.html
-cp ../writing/differential_geometry_in_hott/latex/discrete_gauge_theory/html/* content/writing/towards_gauge_theory_in_hott
-cp ../writing/differential_geometry_in_hott/latex/discrete_gauge_theory/discrete_gauge_theory.pdf content/writing/towards_gauge_theory_in_hott/
-cp ../writing/differential_geometry_in_hott/latex/discrete_gauge_theory/dgt_talk*.pdf content/writing/towards_gauge_theory_in_hott/
-cp content/writing/towards_gauge_theory_in_hott/*.svg public/writing/towards_gauge_theory_in_hott
+TRG=content/writing/differential_geometry_in_hott/index.html
+cp ../writing/differential_geometry_in_hott/latex/discrete_gauge_theory/html/* content/writing/differential_geometry_in_hott
+cp ../writing/differential_geometry_in_hott/latex/discrete_gauge_theory/discrete_gauge_theory.pdf content/writing/differential_geometry_in_hott/
+cp ../writing/differential_geometry_in_hott/latex/discrete_gauge_theory/dgt_talk*.pdf content/writing/differential_geometry_in_hott/
+cp content/writing/differential_geometry_in_hott/*.svg public/writing/differential_geometry_in_hott
 echo "---" > $TRG
 echo 'title: "Discrete differential geometry in homotopy type theory"' >> $TRG
 cat $SRCYML | egrep '^[a-z][a-z]*: ' | egrep -v title >> $TRG
